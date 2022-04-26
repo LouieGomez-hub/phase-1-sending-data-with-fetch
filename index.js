15eb831d36a5479654fecd8194fd7d3c07361f5d
louie_gomez <kinggomez027.lg@gmail.com>
2022-04-25 20:22:54 -0400

    commit tree e8f82eaa11b4118a009e0fc744ea2380269b1953

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.innerHTML = object.id
+            console.log(object);
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+            alert("Unauthorized Access");
+            console.log(error.message);
+        })
+};
